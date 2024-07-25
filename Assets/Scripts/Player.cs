4ed6e1d954b659d9e81ea532d57d68661d5f787b
NAKOBERRY <165394329+NAKOBERRY@users.noreply.github.com>
2024-07-25 09:09:57 +0900

    플레이어 이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rigid;
+    [SerializeField] private int speed;
+    private Vector2 inputVec;
+
+
+
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        inputVec.x = Input.GetAxis("Horizontal");
+        inputVec.y = Input.GetAxis("Vertical");
+
+        MovePlayer();
     }
+
+    public void MovePlayer()
+    {
+        var newInputVec = inputVec * speed * Time.deltaTime;
+
+        Vector2 newPosition = rigid.position + newInputVec;
+
+        rigid.MovePosition(newPosition);
+    }
+
 }
