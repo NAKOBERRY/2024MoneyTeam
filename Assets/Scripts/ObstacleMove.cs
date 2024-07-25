69fe856ba00c4cb0c34c125f505f1a80849b03c8
NAKOBERRY <165394329+NAKOBERRY@users.noreply.github.com>
2024-07-25 13:12:13 +0900

    플레이어 스크립트 수정

---
+++ Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    private Transform m_transform;
+
+    private void Start()
+    {
+        m_transform = GetComponent<Transform>();
+    }
+    
+    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,8 +16,7 @@ public class Player : MonoBehaviour
     [SerializeField] public Vector2 overlabBoxSize;
     [SerializeField] public LayerMask groundLayer;
     private bool isGrounded;
-    private string playerStats = "run";
-
+    
 
     private void Start()
     {
@@ -31,69 +30,20 @@ public class Player : MonoBehaviour
         inputVec.y = Input.GetAxisRaw("Vertical");
         FlipSprite();
         CheckGrounded();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            playerStats = "run";
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            playerStats = "swim";
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            playerStats = "ride";
-        }
-        PlayerStats(playerStats);
+        Jump();
+        PlayerMove();
     }
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Water"))
-        {
-            playerStats = "swim";
-        }
-        else if (collision.gameObject.CompareTag("Ride"))
-        {
-            playerStats = "ride";
-        }
-        else if (collision.gameObject.CompareTag("Run"))
-        {
-            playerStats = "run";
-        }
-    }
+  
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position + bottomOffset, overlabBoxSize);
     }
-
-    private void PlayerStats(string playerStats)
-    {
-
-        switch (playerStats)
-        {
-            case "run":
-                PlayerMove();
-                Jump();
-                break;
-
-            case "swim":
-                IsSwim();
-                break;
-
-            case "ride":
-                IsRide();
-                Jump();
-                break;
-        }
-    }
-
+    
     //점프
     public void Jump()
     {
-
         if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded)
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
@@ -120,24 +70,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    //수영 함수
-    public void IsSwim()
-    {
-        rigid.gravityScale = 0;
-        transform.Translate(Vector2.up * inputVec.y * speed * Time.fixedDeltaTime);
-        transform.Translate(Vector2.right * inputVec.x * speed * Time.fixedDeltaTime);
-    }
-
-    //자전거 함수
-    public void IsRide()
-    {
-        var rideSpeed = speed + 3f;
-        rigid.gravityScale = 2;
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * inputVec.x * rideSpeed * Time.fixedDeltaTime);
-        }     
-    }
 
     //플레이어 기본 이동
     public void PlayerMove()
