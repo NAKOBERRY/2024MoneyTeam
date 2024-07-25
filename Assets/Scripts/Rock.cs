380b06339fd373e8072635954a95dcfa34d488fc
mincat0126 <tearswood@gmail.com>
2024-07-25 14:26:05 +0900

    돌 스크립트

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -4,9 +4,41 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    private Rigidbody2D rigid;
+    private BoxCollider2D boxCollider;
+
+    private int digCount=2;
+    private bool isDig=false;
+    private void Awake()
+    {
+        rigid=GetComponent<Rigidbody2D>();
+        boxCollider=GetComponent<BoxCollider2D>();
+    }
+    private void Start()
+    {
+        DigRock();
+    }
+    private void Update()
+    {
+        if (isDig == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                --digCount;
+            }
+        }
+        if (digCount <= 0&&isDig==true)
+        {
+            float x = Random.Range(-20, 21);
+            float y = Random.Range(5, 10);
+            rigid.AddForce(new Vector2(x, y), ForceMode2D.Impulse);
+            boxCollider.isTrigger = true;
+            isDig = false;  
+        }
+    }
     public void DigRock()
     {
-        int digCount = Random.Range(10, 16);
-
+        digCount = Random.Range(10, 16);
+        isDig= true;
     }
 }
